268360ad7d453b4018cf703efa0c37e5e0a20e2e
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-04-14 13:47:59 -0300

    Navegacao inferior

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -1,49 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricao.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca()
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData( // cor e opacidade de todos os icones
-          color: Colors.grey,
-          opacity:0.8
-        ),
-        title: Image.asset(
-            "images/youtube.png",
-             width: 98,
-             height: 22,
+        iconTheme: IconThemeData(
+            color: Colors.grey
         ),
         backgroundColor: Colors.white,
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
         actions: <Widget>[
           IconButton(
-              onPressed: (){
-
-              },
-              icon: Icon(Icons.videocam)
+            icon: Icon(Icons.videocam),
+            onPressed: (){
+              print("acao: videocam");
+            },
           ),
           IconButton(
-              onPressed: (){
-
-              },
-              icon: Icon(Icons.search)
+            icon: Icon(Icons.search),
+            onPressed: (){
+              print("acao: pesquisa");
+            },
           ),
           IconButton(
-              onPressed: (){
-
-              },
-              icon: Icon(Icons.account_circle)
+            icon: Icon(Icons.account_circle),
+            onPressed: (){
+              print("acao: conta");
+            },
           )
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indiceAtual,
+          onTap: (indice){
+            setState(() {
+              _indiceAtual = indice; // pega o click do usuario para mudar a cor do icone
+            });
+          },
+          type: BottomNavigationBarType.fixed, //fixed > 3 BottomNavigationBarItem()
+          fixedColor: Colors.red,              //shifting <=3 BottomNavigationBarItem()
+          items: [
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.orange,
+                icon: Icon(Icons.home),
+                label: "Inicio"
+               // label: Text("Início")
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.red,
+              //title: Text("Em alta"),
+                icon: Icon(Icons.whatshot),
+                label: "Em Alta"
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.blue,
+              //title: Text("Inscrições"),
+                icon: Icon(Icons.subscriptions),
+                label: "Inscrições"
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.green,
+              //title: Text("Biblioteca"),
+                icon: Icon(Icons.folder),
+                label: "Biblioteca"
+            ),
+          ]
+      ),
     );
   }
 }
--- youtube/lib/main.dart
+++ youtube/lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:youtube/Home.dart';
 
