0bfecb10d14901d2ee48caf2fd93e9441c5a0b15
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-04-17 20:14:31 -0300

    interface do app

---
+++ minhas_anotacoes/android/app/src/main/kotlin/cursoflutterapp/minhas_anotacoes/MainActivity.kt
@@ -0,0 +1,6 @@
+package cursoflutterapp.minhas_anotacoes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ minhas_anotacoes/lib/Home.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _descricaoController = TextEditingController();
+
+  _exibirTelaCadastro(){
+
+    showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Adicionar anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min, // tamanho do item column utilizado
+              children: <Widget>[
+                TextField(
+                  controller: _tituloController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Título",
+                      hintText: "Digite título..."
+                  ),
+                ),
+                TextField(
+                  controller: _descricaoController,
+                  decoration: InputDecoration(
+                      labelText: "Descrição",
+                      hintText: "Digite descrição..."
+                  ),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: ()=> Navigator.pop(context),
+                  child: Text("Cancelar")
+              ),
+              TextButton(
+                  onPressed: () {
+
+                    Navigator.pop(context)
+                  },
+                  child: Text("Salvar")
+              )
+            ],
+          );
+        }
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas anotações"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          child: Icon(Icons.add),
+          onPressed: (){
+            _exibirTelaCadastro();
+          }
+      ),
+    );
+  }
+}
